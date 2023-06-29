2accecd90e77653c6be470c0836b015bc7a0867a
Sean McGrath <smcgrath@hsph.harvard.edu>
2023-06-29 11:29:10 -0400

    Added argument "include_scb" in plot.STE_S

--- R/s3methods.R
+++ R/s3methods.R
@@ -5,6 +5,7 @@
 #' @param x Object of class "STE_S".
 #' @param header character string specifying the title of the column of subgroup names
 #' @param xlab character string specifying the title for the x-axis.
+#' @param include_scb logical scalar specifying whether to include simultaneous 95\% confidence bands in the forest plot
 #' @param ... Other arguments, which are passed to \code{\link[metafor]{forest.rma}}.
 #' @return No value is returned.
 #' @seealso \code{\link{CMetafoR.STE.S}}
@@ -13,7 +14,8 @@
 #'
 #' @export
 
-plot.STE_S <- function(x, header = 'Subgroup', xlab = 'Treatment Effect', ...){
+plot.STE_S <- function(x, header = 'Subgroup', xlab = 'Treatment Effect',
+                       include_scb = TRUE, ...){
   if (!inherits(x, "STE_S")){
     stop("Argument 'x' must be an object of class \"STE_S\".")
   }
@@ -27,9 +29,11 @@ plot.STE_S <- function(x, header = 'Subgroup', xlab = 'Treatment Effect', ...){
          xlab = xlab,
          ...)
 
-  for (i in 1:(x$n_x_tilde * x$no_S)) {
-    graphics::segments(x0 = x$plot_scb[i, 1],
-                       y0 = x$n_x_tilde * x$no_S + 1 - i,
-                       x1 = x$plot_scb[i, 2])
+  if (include_scb){
+    for (i in 1:(x$n_x_tilde * x$no_S)) {
+      graphics::segments(x0 = x$plot_scb[i, 1],
+                         y0 = x$n_x_tilde * x$no_S + 1 - i,
+                         x1 = x$plot_scb[i, 2])
+    }
   }
 }
