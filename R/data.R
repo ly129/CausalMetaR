7f696511f62c11c273d1f5235a1f03ccb2f57bc6
Sean McGrath <smcgrath@hsph.harvard.edu>
2023-12-28 23:02:11 -0500

    Revised documentation of the example data sets

--- R/data.R
+++ R/data.R
@@ -1,31 +1,33 @@
-#' Multi-source data
+#' Multi-source dataset
 #'
-#' Simulated multi-source dataset for CMetafoR
+#' Simulated multi-source dataset consisting of 3 sources.
 #'
-#' @format `dat_multisource`
-#' A data frame with 3917 rows and 13 columns
-#' \describe{
-#'  \item{EM}{Effect modifier}
-#'  \item{X2, ..., X10}{9 covariates}
-#'  \item{S}{Source}
-#'  \item{A}{Treatment: 1 - treated; 0 - untreated}
-#'  \item{Y}{Outcome}
+#' @docType data
+#'
+#' @format A data frame with 3,917 rows and 13 columns. The columns are:
+#' \tabular{ll}{
+#'  \code{EM} \tab Effect modifier. \cr
+#'  \code{X2}, ..., \code{X10} \tab Covariates. \cr
+#'  \code{S} \tab Source indicator. \cr
+#'  \code{A} \tab Treatment (1 for treated and 0 for untreated). \cr
+#'  \code{Y} \tab Outcome.
 #' }
 "dat_multisource"
 
 
 
-#' External data
+#' External dataset
 #'
-#' Simulated external dataset for CMetafoR
+#' Simulated external dataset.
 #'
-#' @format `dat_external`
-#' A data frame with 10083 rows and 13 columns
-#' \describe{
-#'  \item{EM}{Effect modifier}
-#'  \item{X2, ..., X10}{9 covariates}
-#'  \item{S}{Source}
-#'  \item{A}{Treatment: 1 - treated; 0 - untreated}
-#'  \item{Y}{Outcome}
+#' @docType data
+#'
+#' @format A data frame with 10,083 rows and 13 columns. The columns are:
+#' \tabular{ll}{
+#'  \code{EM} \tab Effect modifier. \cr
+#'  \code{X2}, ..., \code{X10}  \tab  Covariates. \cr
+#'  \code{S} \tab Source indicator. \cr
+#'  \code{A} \tab Treatment (1 for treated and 0 for untreated). \cr
+#'  \code{Y} \tab Outcome.
 #' }
 "dat_external"
